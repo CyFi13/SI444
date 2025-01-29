75980c3a1ecc851975dbbc70c0af80d37f915932
CyFi13 <m260102@usna.edu>
2025-01-29 11:39:10 -0500

    msg

--- A1/app/src/main/java/edu/usna/mobileos/a1_alvistristen/MainActivity.kt
+++ A1/app/src/main/java/edu/usna/mobileos/a1_alvistristen/MainActivity.kt
@@ -2,10 +2,13 @@ package edu.usna.mobileos.a1_alvistristen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+
 }
--- C7/MenuExamples/app/src/main/java/edu/usna/mobileos/menuexamples/MainActivity.kt
+++ C7/MenuExamples/app/src/main/java/edu/usna/mobileos/menuexamples/MainActivity.kt
@@ -1,6 +1,12 @@
 package edu.usna.mobileos.menuexamples
 
 import android.os.Bundle
+import android.view.ContextMenu
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Button
+import android.widget.Toolbar
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,37 +16,50 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.android.material.snackbar.Snackbar
 import edu.usna.mobileos.menuexamples.ui.theme.MenuExamplesTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            MenuExamplesTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+        setContentView(R.layout.activity_main)
+
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        menuInflater.inflate(R.menu.options_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle item selection
+        return when (item.itemId) {
+            R.id.show -> {
+                Snackbar.make(findViewById(android.R.id.content), "Show pressed", Snackbar.LENGTH_LONG).show()
+                // do stuff here
+                true
             }
+            R.id.forward -> {
+                // do stuff here
+                Snackbar.make(findViewById(android.R.id.content), "Forward pressed", Snackbar.LENGTH_LONG).show()
+                true
+            }
+            R.id.back -> {
+                // do stuff here
+                Snackbar.make(findViewById(android.R.id.content), "Backward pressed", Snackbar.LENGTH_LONG).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MenuExamplesTheme {
-        Greeting("Android")
+    override fun onCreateContextMenu(
+        menu: ContextMenu?,
+        v: View?,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        menuInflater.inflate(R.menu.context_menu, menu)
     }
+
 }
