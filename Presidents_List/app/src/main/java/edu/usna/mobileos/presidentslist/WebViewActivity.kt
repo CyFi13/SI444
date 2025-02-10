7f4c18ef2f05540db0c7ae449d6e9155615b283c
CyFi13 <m260102@usna.edu>
2025-02-10 10:43:30 -0500

    msg

---
+++ Presidents_List/app/src/main/java/edu/usna/mobileos/presidentslist/WebViewActivity.kt
@@ -0,0 +1,11 @@
+package edu.usna.mobileos.presidentslist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WebViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_web_view)
+    }
+}
