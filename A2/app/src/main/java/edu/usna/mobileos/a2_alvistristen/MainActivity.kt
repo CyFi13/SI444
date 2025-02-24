a20bfd7774e2dd4550c7712958d90074e68179e5
CyFi13 <m260102@usna.edu>
2025-02-24 10:42:01 -0500

    msg

--- A2/app/src/main/java/edu/usna/mobileos/a2_alvistristen/MainActivity.kt
+++ A2/app/src/main/java/edu/usna/mobileos/a2_alvistristen/MainActivity.kt
@@ -2,10 +2,29 @@ package edu.usna.mobileos.a2_alvistristen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.webkit.WebView
+import android.widget.Button
+import android.widget.EditText
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    val webView : WebView = WebView(findViewById(R.id.url_page))
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val goButton: Button = findViewById(R.id.url_button)
+        goButton.setOnClickListener(this)
+
+        webView.loadUrl("https://www.apple.com/")
+    }
+
+    override fun onClick(v: View?) {
+        val url: String = "https://apple.com"
+        when(v?.id){
+            R.id.url_button -> {
+                webView.loadUrl(url)
+            }
+        }
     }
 }
