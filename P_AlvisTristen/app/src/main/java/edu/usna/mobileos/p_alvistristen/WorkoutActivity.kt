f38a37aef592cec11dc2d60cf53170f3ce9a88f2
CyFi13 <m260102@usna.edu>
2025-04-18 11:32:35 -0400

    msg

--- P_AlvisTristen/app/src/main/java/edu/usna/mobileos/p_alvistristen/WorkoutActivity.kt
+++ P_AlvisTristen/app/src/main/java/edu/usna/mobileos/p_alvistristen/WorkoutActivity.kt
@@ -3,49 +3,42 @@ package edu.usna.mobileos.p_alvistristen
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.ViewGroup
+import android.view.ViewGroup.LayoutParams
 import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.ScrollView
 import android.widget.TextView
 
 class WorkoutActivity : AppCompatActivity() {
-    lateinit var scrollView: ScrollView
+    lateinit var layout: LinearLayout
     lateinit var routine: Routine
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_workout)
-
         // set up main viewing window
-        scrollView = findViewById(R.id.routine_instructions_view)
+        layout = findViewById(R.id.routine_layout)
 
         // TODO: recieve intent containing routine data from searchActivity and place that into list
         // test
         val sets = ArrayList<Set>()
-        for(i in (0..30)) { // testing recyclerView
-            var set: Set = Set("test1", )
-
+        for(i in (0..50)) { // testing recyclerView
+            var exercise: Exercise = Exercise("title", "description")
+            var set: Set = Set(exercise, 10)
             sets.add(set)
         }
-        var routine: Routine = Routine("test", "", sets)
+        routine = Routine("test", "", sets)
 
         // TODO: build up the scroll view with linear layouts to display the workout instructions
         buildInstructions()
+
     }
 
     private fun buildInstructions() {
-        for(exercise in routineList) {
-            val layout = LinearLayout(this)
+        for(set in routine.routine) {
             val textView = TextView(this)
-
-            textView.text = exercise.description
-
+            textView.layoutParams = ViewGroup.LayoutParams(LayoutParams.MATCH_PARENT, 100)
+            textView.text = set.exercise.title
             layout.addView(textView)
-
-            val layout_parameters = ViewGroup.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT
-            )
-
-            layout.layoutParams = layout_parameters
-        }
+       }
     }
 }
